a07819ace92b410a585ba87b854084011010744f
Ronaldo Reimer <ronaldoreimer68@gmail.com>
2026-05-29 11:11:52 -0300

    Implement hover effect on link cards
    Add hover effect and console message for link cards

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const cards = document.querySelectorAll(".link-card");
 
+cards.forEach(card => {
+
+    card.addEventListener("mouseenter", () => {
+
+        card.style.transition = "0.3s";
+
+    });
+
+});
+
+/* CONSOLE MESSAGE */
+
+console.log("Site carregado com sucesso 🚀");
